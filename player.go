8ec8bd2de39ede5820cb9a8ae69012e59361d5da
RaptorExTolop <jamesappletonret@gmail.com>
2025-09-02 22:23:12 +0930

    I'm close, but I think the animation renderer needs some more work, I'm trying to reference things that don't exist yet

--- player.go
+++ player.go
@@ -2,6 +2,28 @@ package main
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-type Player struct {
-	Texture rl.Texture2D
+type AnimationPlayer struct {
+	spriteSheet    rl.Texture2D
+	animationNames []string
+	animations     []Animation
+}
+
+type Animation struct {
+	frameSize                       rl.Vector2
+	yIndex, currentFrame, animFrame int32
+}
+
+func (a *Animation) Start() {
+	a.currentFrame = 0
+	a.animFrame = 0
+}
+
+func (a *Animation) Play() {
+
+	a.animFrame++
+}
+
+func (a *Animation) Init(frameSize rl.Vector2, yIndex int32) {
+	a.frameSize = frameSize
+	a.yIndex = yIndex
 }
